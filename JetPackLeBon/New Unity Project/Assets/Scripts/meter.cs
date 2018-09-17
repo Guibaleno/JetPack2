1d35a67e619c6ed9ed7857082093a9601b2a65f1
B533P-28\533 <eliot.eliot@hotmail.com>
2018-09-17 16:01:01 -0400

    2e commit

---
+++ JetPackLeBon/New Unity Project/Assets/Scripts/meter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class meter : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private Transform checkpoint;
+
+
+    [SerializeField]
+    private Text distanceText;
+
+    private float distance;
+
+  
+    private void Update()
+    {
+
+        distance = transform.position.x;
+
+
+        distanceText.text = "Distance: " + distance.ToString("F1") + " meters";
+
+ 
+
+    }
+}
---
+++ JetPackLeBon/New Unity Project/Assets/Scripts/playerdistance.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerdistance : MonoBehaviour {
+
+
+    private Rigidbody2D rb;
+
+    private float dirX, moveSpeed = 5f;
+
+
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+
+        dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+
+        rb.velocity = new Vector2(dirX, rb.velocity.y);
+    }
+}
